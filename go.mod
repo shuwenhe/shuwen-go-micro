0a8aad6ed324a80c9def942d4457b7ce895a40df
ShuwenHe <1201220707@pku.edu.cn>
2021-02-08 12:46:57 +0800

    feat:NewService

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module shuwen-go-micro
 
 go 1.15
+
+require github.com/micro/go-micro v1.18.0
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/micro/go-micro/web"
+)
+
+func main() {
+	add := func(o *web.Options) {
+		o.Address = ":8001"
+	}
+	server := web.NewService(add)
+	fmt.Println("server = ", server)
+}
